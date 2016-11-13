7b804fc0a7f4ce105aeb81dff430e878dd4e259a
Konrad Śliżewski <konrad.slizewski@gmail.com>
2016-11-13 18:48:52 +0100

    Stworzenie modelu, slugów oraz widoku listy

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,30 @@
-from django.db import models
+from unidecode import unidecode
 
-# Create your models here.
+from django.db import models
+from django.template.defaultfilters import slugify
+
+
+class Post(models.Model):
+    author = models.ForeignKey('User', on_delete=models.CASCADE)
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(unique=True, blank=True)
+    created = models.DateTimeField(auto_now=False, auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True, auto_now_add=False)
+    content = models.TextField()
+
+    # create slug
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.slug = slugify(unidecode(self.title))
+        super(Post, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return str(self.title)
+
+
+class User(models.Model):
+    first_name = models.CharField(max_length=250)
+    last_name = models.CharField(max_length=250)
+
+    def __str__(self):
+        return str(self.first_name + " " + self.last_name)
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.views.generic.list import ListView
 
-# Create your views here.
+from .models import Post
+
+
+class PostListView(ListView):
+    model = Post
+
+    def get_context_data(self, **kwargs):
+        context = super(PostListView, self).get_context_data(**kwargs)
+        return context
+
+        # TODO: CRUD, Widok szczegółowy postu
